b24a551a0baf7bb86f7fdf262f7a9066309633b2
tommybrightly <tommybrightly@gmail.com>
2025-11-07 06:57:08 -0600

    added navigation to home screen

--- stressedout/App.tsx
+++ stressedout/App.tsx
@@ -1,20 +1,20 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import "react-native-gesture-handler";
+import { NavigationContainer } from "@react-navigation/native";
+import Tabs from "@/navigation/Tabs";
+import { StatusBar } from "expo-status-bar";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { View } from "react-native";
+
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+return (
+<NavigationContainer>
+<SafeAreaView style={{ flex: 1 }}>
+<View style={{ flex: 1 }}>
+<Tabs />
+<StatusBar style="auto" />
+</View>
+</SafeAreaView>
+</NavigationContainer>
+);
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
