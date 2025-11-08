0e9da8ff9476ccb5c9ec9586476e5d734453148c
tommybrightly <tommybrightly@gmail.com>
2025-11-08 13:02:37 -0600

    updated eas info

--- app.config.ts
+++ app.config.ts
@@ -1,25 +1,25 @@
-import { ExpoConfig } from "expo/config";
+// app.config.ts
+import { ConfigContext, ExpoConfig } from "expo/config";
 
-
-const config: ExpoConfig = {
-name: "Stress Less",
-slug: "StressLess",
-scheme: "stressless",
-version: "1.0.0",
-orientation: "portrait",
-userInterfaceStyle: "automatic",
-ios: {
-supportsTablet: true,
-bundleIdentifier: "com.tbrightlty.stressless"
-},
-android: {
-softwareKeyboardLayoutMode: "resize",
-package: "com.tbrightly.stressless",
-versionCode: 1,
-adaptiveIcon: {
-backgroundColor: "#FFFFFF"
-}
-},
-
-};
-export default config;
+export default ({ config }: ConfigContext): ExpoConfig => ({
+  ...config,
+  name: "Stress Less",
+  slug: "StressLess",
+  scheme: "stressless",
+  ios: {
+    ...config.ios,
+    bundleIdentifier: "com.tommybrightly.stressless",
+    buildNumber: "1",
+  },
+  android: {
+    ...config.android,
+    package: "com.tommybrightly.stressless",
+    versionCode: 1,
+  },
+  extra: {
+    ...config.extra,
+    eas: {
+      projectId: "39d3af39-1bb1-401c-94bb-50a203935ce0", // <- REQUIRED
+    },
+  },
+});
